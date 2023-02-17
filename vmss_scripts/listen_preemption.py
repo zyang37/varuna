60fc6fcd26a5f77d14bf2f9b63c63ebe48a5f950
Zhenning Yang <znyang@ampere03.eecs.umich.edu>
2023-02-17 13:45:31 -0500

    ...

--- vmss_scripts/listen_preemption.py
+++ vmss_scripts/listen_preemption.py
@@ -4,7 +4,7 @@ import json
 import socket
 import urllib.request
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 metadata_url = "http://169.254.169.254/metadata/scheduledevents?api-version=2019-01-01"
 this_host = socket.gethostname()
@@ -15,32 +15,45 @@ def client(ip, port, message):
         sock.sendall(bytes(message, 'ascii'))
 
 def get_scheduled_events():
+    data = {}
     req = urllib.request.Request(metadata_url)
     req.add_header('Metadata', 'true')
-    resp = urllib.request.urlopen(req)
-    data = json.loads(resp.read())
+    try:
+        resp = urllib.request.urlopen(req, timeout=1)
+        data = json.loads(resp.read())
+    except:
+        pass
     return data
 
 
 def handle_scheduled_events(data, ip, port):
-    for evt in data['Events']:
-        eventid = evt['EventId']
-        status = evt['EventStatus']
-        resources = evt['Resources']
-        eventtype = evt['EventType']
-        resourcetype = evt['ResourceType']
-        notbefore = evt['NotBefore'].replace(" ", "_")
-        print(datetime.now())
-        print("+ Scheduled Event. This host " + this_host + " is scheduled for " + eventtype + " not before " + notbefore)
-        # Logic for handling event, send morph signal to master
-        print(this_host in resources)
-        client(ip, port, "preempt {}".format(notbefore))
+    # for evt in data['Events']:
+    #     eventid = evt['EventId']
+    #     status = evt['EventStatus']
+    #     resources = evt['Resources']
+    #     eventtype = evt['EventType']
+    #     resourcetype = evt['ResourceType']
+    #     notbefore = evt['NotBefore'].replace(" ", "_")
+    #     print(datetime.now())
+    #     print("+ Scheduled Event. This host " + this_host + " is scheduled for " + eventtype + " not before " + notbefore)
+    #     # Logic for handling event, send morph signal to master
+    #     print(this_host in resources)
+    #     client(ip, port, "preempt {}".format(notbefore))
+
+    ## "%a,_%d_%b_%Y_%H:%M:%S_%Z"
+    print(datetime.now())
+    notbefore = datetime.now() + timedelta(seconds=3)
+    notbefore = notbefore.strftime("%a,_%d_%b_%Y_%H:%M:%S_%Z") + "UTC"
+    client(ip, port, "preempt {}".format(notbefore)) 
 
 def main():
-    ip, port = "10.0.3.4", 4200       # manager IP
+    # ip, port = "10.0.3.4", 4200       # manager IP
+    ip, port = "172.17.0.2", 4200       # manager IP
     while True:
         data = get_scheduled_events()
+        print("data:", data)
         handle_scheduled_events(data, ip, port)
+        break
         time.sleep(5)
 
 if __name__ == '__main__':
--- vmss_scripts/morph_client.py
+++ vmss_scripts/morph_client.py
@@ -11,7 +11,8 @@ def client(ip, port, message):
 
 def get_available_machines():
     # gets reachable machines
-    bash_out = os.popen("bash /home/varuna/t-nisar/Megatron-LM/get_available_machines.sh").read()
+    # bash_out = os.popen("bash /home/varuna/t-nisar/Megatron-LM/get_available_machines.sh").read()
+    bash_out = os.popen("bash /home/znyang/varuna/varuna/get_available_machines.sh").read()
     machines = bash_out.split("\n")
     if machines[-1] == "":
         machines = machines[:-1]
@@ -21,21 +22,24 @@ def get_available_machines():
 
 if __name__ == "__main__":
 
-    server_ip = "172.16.5.4"
+    server_ip = "172.17.0.2"
     server_port = 4200
 
-    with open("/home/varuna/t-nisar/Megatron-LM/nservers","r") as f:
-        current_num_machines = int(f.read())
+    # with open("/home/varuna/t-nisar/Megatron-LM/nservers","r") as f:
+    #     current_num_machines = int(f.read())
+    current_num_machines = 2
 
-    new_machines = get_available_machines()
+    # new_machines = get_available_machines()
+    new_machines = ["172.17.0.4"]
     print(new_machines)
-    if len(new_machines) % 2 != 0:
-        new_machines = new_machines[:-1]
+    # if len(new_machines) % 2 != 0:
+    #     new_machines = new_machines[:-1]
+    #     print(new_machines)
 
     if len(new_machines) == current_num_machines:
         print(str(datetime.now()), "no morph")
     else:
-        of = open("/home/varuna/t-nisar/Megatron-LM/available_machines.out","w")
+        of = open("/home/znyang/varuna/examples/Megatron-LM/available_machines.out","w")
         for m in new_machines:
             of.write(m)
             if "\n" not in m:
